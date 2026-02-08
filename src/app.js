f4fcc71acb6bb52cca43660e1d5ac3b866cdcfe0
Mirodil <mirodil.mavlonov.2022@gmail.com>
2026-02-09 01:36:21 +0500

    committ

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import categoryRoutes from './routes/category.routes.js';
 import tagRoutes from './routes/tag.routes.js';
 import reactionRoutes from './routes/reaction.routes.js';
 import viewRoutes from './routes/view.routes.js';
-
+import authRoutes from './routes/auth.routes.js'; // 1. Auth routes qo'shildi
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,14 +16,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // form-data uchun foydali
+app.use(express.urlencoded({ extended: true }));
 
 // Statik papka
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Routes
-app.use('api/posts', postRoutes);
-app.use('api/categories', categoryRoutes);
+// Routes (Barchasiga / qo'shildi)
+app.use('/api/auth', authRoutes);       // 2. Auth endpointi qo'shildi
+app.use('/api/posts', postRoutes);      // 3. / qo'shildi
+app.use('/api/categories', categoryRoutes); // 4. / qo'shildi
 app.use('/api/tags', tagRoutes);
 app.use('/api/reactions', reactionRoutes);
 app.use('/api/views', viewRoutes);
