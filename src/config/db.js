23f7cf3948c520f6f12eb61c2e35bf830b9a4e94
Mirodil <mirodil.mavlonov.2022@gmail.com>
2026-02-08 18:51:29 +0500

    commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,18 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Neon uchun bu juda muhim
+  }
+});
+
+pool.on('connect', () => {
+  console.log("🐘 Neon PostgreSQL-ga muvaffaqiyatli ulandi!");
 });
 
 export default pool;
