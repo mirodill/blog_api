3cdebc8c486f470b5f25e0f432fbf4cc7f665e53
Mirodil <mirodil.mavlonov.2022@gmail.com>
2026-02-14 00:02:02 +0500

    Added axios for self-ping

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
-import { httpServer } from './app.js'; // 'app' emas, 'httpServer' ni import qiling
+import { httpServer } from './app.js';
 import dotenv from 'dotenv';
+import axios from 'axios'; // npm install axios qiling
 
 dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-// BU YERDA: 'app.listen' emas, 'httpServer.listen' bo'lishi shart!
 httpServer.listen(PORT, () => {
-  console.log(`🚀 Server va Socket http://localhost:${PORT} da ishladi`);
+  console.log(`🚀 Server running on port ${PORT}`);
+  
+  // Har 10 daqiqada o'ziga ping yuborish (Server o'chib qolmasligi uchun)
+  setInterval(() => {
+    axios.get(`https://blog-api-uzfl.onrender.com/api/v1/auth/me`) // O'zingizning Render URLingizni yozing
+      .then(() => console.log('Self-ping success'))
+      .catch((err) => console.log('Self-ping error:', err.message));
+  }, 600000); // 10 daqiqa
 });
